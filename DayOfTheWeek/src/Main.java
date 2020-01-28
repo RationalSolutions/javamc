f25e5d3b6f1dd5a50d890bd31d63c0ebaad89cdf
cr4ck3d <coby@azjenkins.com>
2020-01-27 18:46:02 -0700

    Made changes

--- DayOfTheWeek/src/Main.java
+++ DayOfTheWeek/src/Main.java
@@ -23,6 +23,8 @@ public class Main {
         ifElsePrintDayOfTheWeek(6);
 
     }
+
+    //something changes
     /*
     Write a method with the name printDayOfTheWeek that has one parameter of type int and name it day.
     The method should not return any value (hint: void)
